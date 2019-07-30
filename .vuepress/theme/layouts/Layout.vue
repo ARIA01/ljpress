d0a6ee526b8570f24d05991dcc0ed3e0dec632d6
ARIA01 <1527226118@qq.com>
2019-07-30 15:19:55 +0800

    添加App介绍文章

--- .vuepress/theme/layouts/Layout.vue
+++ .vuepress/theme/layouts/Layout.vue
@@ -1,12 +1,6 @@
 <template>
-  <div
-    class="theme-container"
-    @touchstart="onTouchStart"
-    @touchend="onTouchEnd"
-  >
-    <Navbar
-      :isShowSidebarButton="isShowSidebarButton"
-    />
+  <div class="theme-container">
+    <Navbar :isShowSidebarButton="isShowSidebarButton" />
 
     <div
       class="custom-layout"
@@ -79,28 +73,6 @@ export default {
     this.$router.afterEach(() => {
       nprogress.done()
     })
-  },
-
-  methods: {
-    // side swipe
-    onTouchStart (e) {
-      this.touchStart = {
-        x: e.changedTouches[0].clientX,
-        y: e.changedTouches[0].clientY
-      }
-    },
-
-    onTouchEnd (e) {
-      const dx = e.changedTouches[0].clientX - this.touchStart.x
-      const dy = e.changedTouches[0].clientY - this.touchStart.y
-      if (Math.abs(dx) > Math.abs(dy) && Math.abs(dx) > 40) {
-        if (dx > 0 && this.touchStart.x <= 80) {
-          this.toggleSidebar(true)
-        } else {
-          this.toggleSidebar(false)
-        }
-      }
-    }
   }
 }
 </script>
